a0e0b75938104571070aff5d338790c6546f1bcb
feleshia33 <feleshiawest@gmail.com>
2020-06-18 14:46:25 -0400

    initial commit

---
+++ Project1_Feleshia_West.py
@@ -0,0 +1,8 @@
+name = input('Enter your name? ')
+address = input('Enter your address with city, state, and zip? ')
+number = input('Enter your number? ')
+college_major = input('Enter your major? ')
+print('name:', name)
+print('address:', address)
+print('number:', number)
+print('college_major:', college_major)
