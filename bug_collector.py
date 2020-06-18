315abd34b12123ca1b4c9b0cdec451191ba69c3a
feleshia33 <feleshiawest@gmail.com>
2020-06-18 16:39:28 -0400

    initial commit

---
+++ bug_collector.py
@@ -0,0 +1,16 @@
+#Exercise #1: Bug Collector
+
+for num in [6]:
+    one = int(input('How many bugs for day 1: '))
+    two = int(input('How many bugs for day 2: '))
+    three = int(input('How many bugs for day 3: '))
+    four = int(input('How many bugs for day 4: '))
+    five = int(input('How many bugs for day 5: '))
+
+    print(int(one + two + three + four + five))
+
+
+    
+
+    
+   
