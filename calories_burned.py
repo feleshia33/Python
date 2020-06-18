00683777c15858328a3d4b5b9509a6a92f68f29e
feleshia33 <feleshiawest@gmail.com>
2020-06-18 17:00:48 -0400

    initial commit

---
+++ calories_burned.py
@@ -0,0 +1,10 @@
+#Exercise #2: Calories Burned
+
+start = 10
+end = 35
+increment = 5
+per_minute = 4.2
+
+for num in range(start, end, increment):
+    min = num * per_minute 
+    print(min)
---
+++ target_variables.py
@@ -0,0 +1,14 @@
+
+
+START_SPEED = 60
+END_SPEED = 131
+INCREMENT = 10
+CONVERSION_FACTOR = 0.6214
+
+print('KPH\tMPH')
+print('--------------')
+
+for kph in range(START_SPEED, END_SPEED, INCREMENT)
+    mph = kph * CONVERSION_FACTOR
+    print(kph, '\t', format(mph, '.1'))
+
