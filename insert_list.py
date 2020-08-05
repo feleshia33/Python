f48860b7d694fdb70109a5a777ec5ba3769a2691
feleshia33 <feleshia.west@g.austincc.edu>
2020-08-05 17:26:39 -0400

    added new proj

---
+++ insert_list.py
@@ -0,0 +1,11 @@
+def main():
+    names = ['Odie', 'Od', 'Odi']
+
+    print('The list of names before', names)
+
+
+    names.insert(0, "OD")
+    print('The list of names after', names)
+
+main()
+    
