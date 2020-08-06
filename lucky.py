1c6e883c72526174ae4c66dce9c8de5f3994446e
feleshia33 <feleshia.west@g.austincc.edu>
2020-08-06 12:45:38 -0400

    daily proj

---
+++ lucky.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+yrAt = 100 - age
+CURRENT_YR = 2020
+
+oneHund = yrAt + CURRENT_YR
+print(name, "you will be 100 years old in", oneHund, end="")
+print(".")
+
+
+
