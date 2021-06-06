10c3717e508c4f82eb9f59f9140f0018b87218e3
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-05 19:24:03 -0500

    practice

---
+++ practice.py
@@ -0,0 +1,132 @@
+import turtle
+
+
+#string_value = input("How many hours did you work? ")
+#hours = int(string_value) * 37.5
+#print(hours)
+
+#name = input("What is your name? ")
+#age = input("What is your age? ")
+#income = (input("What is your income? "))
+
+#print("Here is the information you entered:")
+#print("Name:", name)
+#print("Age:", age)
+#print("Income:", income)
+
+#salary = 3000 
+#bonus = 1000
+#pay = salary + bonus
+#print(pay)
+
+#sales = float(input("What is the sales amount? "))
+#discount = float(.30 * sales)
+#print("The total amount with the discount with be", (sales - discount))
+
+#total_seconds = float(input("Enter a number in seconds: "))
+
+#hours = total_seconds // 3600
+
+#minutes = (total_seconds // 60) % 60
+
+#seconds = total_seconds % 60
+
+#print("Here is the time in hours, minutes and seconds: ")
+#print("Hours: ", hours)
+#print("Minutes: ", minutes)
+#print("Seconds: ", seconds)
+
+#This is a program that will give you the present value of a future amount.
+
+#future_value = int(input("What is the amount you'd like to save? "))
+#interest_rate = float(input("What is the interest rate? "))
+#years = int(input("How many year? "))
+
+#print((future_value) / (1 + interest_rate)**years)
+
+#Use end="" to suppress endline
+#print("One", end="")
+#print("Two")
+#print("Three")
+
+#Specifying an item seperator
+#print("One", "Two", "Three", sep="*")
+
+#Escape characters
+#print("Hi\nThis is on a new line\tthis is tabbed\n\\This prints a back slash")
+
+#format to two  decimal places
+#amount_due = 5000.00
+#monthly_payment = amount_due / 12.0
+#print("The monthly payment is", format(monthly_payment, ".2f"))
+
+#format to two  decimal places and comma
+#amount_due = 5000.00
+#monthly_payment = amount_due * 12.0
+#print("The monthly payment is", format(monthly_payment, ",.2f"), sep=" ")
+
+#specifying a min field width of floating numbers
+#x = format(12345.6789, "12.2f")
+#print(x)
+
+#num1 = 1223.44
+#print(format(num1, "8.2f"))
+#print(format(num1, "8,.2f"))
+
+#specifying a min field width of integers
+#print(format(123456, "10,d"))
+
+#height = int(input("Enter your height in inches: "))
+
+#if height < 60:
+#   print("Your are really short.")
+#else:
+#   print("You are average height.")
+
+
+#payroll calculator
+#hours_worked = int(input("Enter the number of hours worked: "))
+#hourly_payrate = float(input("Enter your pay rate: "))
+#time_half = (hourly_payrate / 2) + hourly_payrate
+
+#if hours_worked > 40:
+#     pay_OT = ((hours_worked - 40) * time_half) + (40 * hourly_payrate)
+#     print("Your gross pay with OT is $", format(pay_OT, ",.2f"), sep="")
+
+#else:
+#    normal = (hours_worked * hourly_payrate)
+#    print("Your pay without OT is $", format(normal, ",.2f"), sep="")
+
+#This program will check to see if two passwords are the same
+
+#password1 = input("Enter your password: ")
+#password2 = input("Re-enter your password: ")
+
+#if password1 == password2:
+#    print("Passwords match")
+#else:
+#    print("Password do not match")
+
+    
+#password = "dontworryaboutit"
+
+#guess = input("Enter the passcode: ")
+
+#if guess == "dontworryaboutit":
+    print("Password accepted.")
+else:
+    print("Sorry, that is the correct passcode.")
+
+
+
+
+
+
+
+
+
+
+                     
+
+
+                      
