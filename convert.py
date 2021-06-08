a4eef4b9557c0e324d1275c8d50cdddabe1faa2f
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-07 21:51:49 -0500

    Coverter

---
+++ convert.py
@@ -0,0 +1,35 @@
+#This program will convert numbers 1-10 into spanish numbers
+
+one = "uno"
+two = "dos"
+three = "tres"
+four = "cautro"
+five = "cinco"
+six = "seis"
+seven = "siete"
+eight = "ocho"
+nine = "nueve"
+ten = "diez"
+
+number = int(input("Enter a number from one to ten: "))
+if number == 1:
+    print(one)
+elif number == 2:
+    print(two)
+elif number == 3:
+    print(three)
+elif number == 4:
+    print(four)
+elif number == 5:
+    print(five)
+elif number == 6:
+    print(six)
+elif number == 7:
+    print(seven)
+elif number == 8:
+    print(eight)
+elif number == 9:
+    print(nine)
+elif number == 10:
+    print(ten)
+
