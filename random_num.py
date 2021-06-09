f859d001ec23233799ee89bdbff3d88bed129b1e
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-08 22:02:57 -0500

    update random num to 5

--- random_num.py
+++ random_num.py
@@ -6,13 +6,13 @@ from random import randrange
 
 #rand_num = (randrange(6))
 rand_num = 5
-
 guess = int(input("Guess the random number from 1 to 5: "))
-print(rand_num)
+
 
 while guess !=rand_num:
-    print("Sorry that is not the random number, guess again.")
-    int(input("Guess the random number from 1 to 5: "))
-else:
     
-    print("Yea! You've guess the random number of", rand_num)
+    if guess == rand_num:
+        print("Yea! You've guess the random number of", rand_num)
+    else:
+        print("Sorry that is not the random number, guess again.")
+        int(input("Guess the random number from 1 to 5: "))
