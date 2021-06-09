310e23e948478ab3164a8cabdba129f0e4d361e2
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-09 06:44:42 -0500

    final commit

--- random_num.py
+++ random_num.py
@@ -1,10 +1,10 @@
-#This program will guess the magic number
+#This program will take an input from the user to guess the random number
 
 from random import randrange
 
 rand_num = (randrange(6))
 
-guess = int(input("Guess the random number between1 to 5: "))
+guess = int(input("Guess the random number between 1 to 5: "))
 
 
 while guess !=rand_num:
