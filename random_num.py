3a95502efd17bce113c085cacc766501e969c066
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-08 22:22:09 -0500

    updated initial guess

--- random_num.py
+++ random_num.py
@@ -2,17 +2,16 @@
 
 from random import randrange
 
-
-
 #rand_num = (randrange(6))
 rand_num = 5
 guess = int(input("Guess the random number from 1 to 5: "))
 
 
 while guess !=rand_num:
+    print("Sorry that is not the random number, guess again.")
+    int(input("Guess the random number from 1 to 5: "))
     
-    if guess == rand_num:
-        print("Yea! You've guess the random number of", rand_num)
-    else:
-        print("Sorry that is not the random number, guess again.")
-        int(input("Guess the random number from 1 to 5: "))
+if guess == rand_num:
+        print("Yea! You've guess the random number of ", rand_num,"!", sep="")
+    
+        
