40aec9f214b967fe59544b92f7cb2f8d0f399387
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-16 21:57:30 -0500

    squares a number app

---
+++ user_squares.py
@@ -0,0 +1,16 @@
+#This program uses a loop to display a table of numbers and their squares.
+
+#Get the ending limit.
+print("This program displays a list of numbers")
+print("(starting at 1_) and their squares.")
+end = int(input("How high should I go? "))
+
+#Print the tables headings.
+print()
+print("Number\tSquare")
+print("--------------")
+
+#Print the number and their squares.
+for number in range(1, end + 1):
+      square = number**2
+      print(number, "\t", square)
