1b4662696a481488af75d8d0398ff15bbb8a38b9
feleshia33 <feleshia.west@g.austincc.edu>
2021-06-29 20:04:57 -0500

    need while loop

---
+++ budget_tracker.py
@@ -0,0 +1,13 @@
+#Budget Analysis: This program asks the user to enter the amount that he/she
+#has budgeted for a month. A total of the monthly expense will total at the end
+#and give and over/under budget depending on the amount spent.
+
+budget = int(input("Enter your monthly budget amount: "))
+num_expense = int(input("Enter the number of expenses for the month: "))
+total_expense = 0
+
+while total_expense < num_expense:
+    print("text")
+    total_expense = num_expense
+    
+    
