f00ce16f30ed2f576034aaae5dbe3d0eced4ec1d
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-02 17:39:35 -0500

    practice functions

---
+++ function.py
@@ -0,0 +1,9 @@
+#This program is to practice using functions
+
+
+def message():
+    print("If I rule the world,")
+    print("I'd free all my sons..")
+    print("black diamonds and pearls.")
+
+message()
