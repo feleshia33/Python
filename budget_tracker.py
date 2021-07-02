6f2fec76d6610509f377693488f328a6ecb45b0c
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-01 21:02:59 -0500

    added if budget less than zero

--- budget_tracker.py
+++ budget_tracker.py
@@ -3,21 +3,26 @@
 #and give and over/under budget depending on the amount spent.
 
 budget = int(input("Enter your monthly budget amount: "))
+
+while budget <= 0:
+    print("Budget cannot be less that zero. Try again")
+    budget = int(input("Enter your monthly budget amount: "))
+    
 num_expense = int(input("Enter the number of expenses for the month: "))
 total = 0.0
 
 for num in range(num_expense):
     print("Expense", num + 1, end="")
-    number = int(input(":$ "))
-    total = total + number
+    number = int(input(": $ "))
+    total += number
 print("--")
 
 
-print("Your total expenses for the month is $ ", format(total, ",.2f"),".", end="", sep="")
+print("Your total expenses for the month is $", format(total, ",.2f"),".", end="", sep="")
 
 if total > budget:
     print(" You are over budget by ", format(total - budget, ",.2f"), ".", sep="")
 else:
-    print(" Congratulations! You are under budget by $ ", format(budget - total,",.2f"), ".", sep="")
+    print(" Congratulations! You are under budget by $", format(budget - total,",.2f"), ".", sep="")
     
     
