9f8e05ceb25b58d441168241189cc0e3f2e9db8a
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-05 20:46:49 -0500

    practice with functions

---
+++ Old/birds.py
@@ -0,0 +1,17 @@
+#This program demonstrates two functions that have local variables with the
+# same name.
+
+def main():
+    texas()
+    california()
+
+
+def texas():
+    birds = 5000
+    print("Texas has", birds, "birds.")
+
+def california():
+    birds = 8000
+    print("California has", birds, "birds.")
+
+main()
