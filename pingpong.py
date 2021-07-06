83cbbd9096d2213d12281374247b583ced593b50
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-06 18:25:29 -0500

    added pp paddle

---
+++ pingpong.py
@@ -0,0 +1,35 @@
+# Simple Ping Pong in Python
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Ping Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+# Stops window from updating, speeds game
+wn.tracer()
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Padddle B
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(350, 0)
+
+# Ball
+
+
+# Main loop
+while True:
+    wn.update()
