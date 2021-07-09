73dda40dfe60111475b992fff3a36c3464d5028c
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-08 21:02:54 -0500

    quiz game

---
+++ quiz.py
@@ -0,0 +1,7 @@
+#This program is a 5 question quiz and answer.
+
+print("Welcome to the 5 question quiz!")
+play = input("Would you like to play? Type 'y' for Yes or 'n' for No. ")
+if play == "y" or "Y":
+    print("Let's beginning.")
+    print("Question 1: What is the capital of Texas? ")
