740f24dd80090b5778ee9baaf56a893ee63667ec
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-13 21:10:26 -0500

    argument practice

---
+++ keyword.py
@@ -0,0 +1,13 @@
+# This program demostrates passing two strings as a keyword
+# argument to a function
+
+def main():
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    print("Your name reversed is ")
+    reverse_name(last = last_name, first = first_name)
+
+def reverse_name(first, last):
+    print(last, first)
+
+main()
