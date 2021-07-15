2ce661e5bfd89cbf766adbdd56d09ca568ed0d12
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-15 18:15:57 -0500

    need to update contrib

--- retirement.py
+++ retirement.py
@@ -5,18 +5,20 @@ CONTRIBUTION_RATE = 0.05
 def main():
     gross_pay = float(input("Enter the gross pay: "))
     bonus = float(input("Enter the amount of bonuses:"))
-    show_pay_contrib(gross_pay)
-    show_bonus_contrib(bonus)
+    show_pay(gross_pay)
+    show_bonus(bonus)
 
 # The show_pay_contrib function accepts the gross pay
 # as an agrument and displays the retirement
 # contribution for hte total amount of pay.
 
-def show_pay_contrib(gross):
+def show_pay(gross):
     contrib = gross * CONTRIBUTION_RATE
     print("Contribution for gross pay: $", format(contrib, ".2f"), sep="")
 
 
+def show_bonus(bonus):
+    print("Contribution for bonuses: $", format(contrib, ".2f"), sep="")
 
 
 main()
