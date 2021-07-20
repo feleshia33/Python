26ec477518ff8fac38302dfb7113e40fad837e99
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-20 06:43:25 -0500

    age app incomplete

---
+++ total_ages.py
@@ -0,0 +1,17 @@
+#This program calculates total ages
+
+total = 0
+age = "y"
+age = input("Enter another age? (y) for yes ")
+
+while age =="y" or age == "Y":
+
+    def main():
+        age = input("Enter another age? (y) for yes ")
+        add = input("Enter an age: ")
+        if total < add:
+            total += 1
+    age = input("Enter another age? (y) for yes")
+            
+
+main()    
