f5f193a199d25b00df711f6f7deb3e53f2d6e740
feleshia33 <feleshia.west@g.austincc.edu>
2021-07-21 07:30:46 -0500

    age app incomplete

--- total_ages.py
+++ total_ages.py
@@ -1,17 +1,25 @@
 #This program calculates total ages
 
-total = 0
-age = "y"
-age = input("Enter another age? (y) for yes ")
+def main():
+    #Get the user's age
+    first_age = int(input("Enter your age: "))
 
-while age =="y" or age == "Y":
+    #Get the user's best friend's age
+    seconf_age = int(input("Enter your friend's age: "))
 
-    def main():
-        age = input("Enter another age? (y) for yes ")
-        add = input("Enter an age: ")
-        if total < add:
-            total += 1
-    age = input("Enter another age? (y) for yes")
-            
+    #Get the sum of both ages
+    total = sum(first_age, second_age)
 
-main()    
+    #Display the total age
+    print("Together you are", total, "years old.")
+
+#The sum function accepts two numberic arguments and
+# return te sum of those arguments.
+
+def sum(num1, num2):
+        result = num1 + num2
+        return result
+
+#Call the main function
+main()
+    
