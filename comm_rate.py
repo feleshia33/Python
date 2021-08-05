71349ba744ae383857f887e102b493158d1a0484
feleshia33 <feleshia.west@g.austincc.edu>
2021-08-04 21:44:28 -0500

    need to update float error

--- comm_rate.py
+++ comm_rate.py
@@ -44,5 +44,26 @@ def get_advanced_pay():
     #Return the amount entered.
     return advanced
 
+# This determine_comm_rate function accepts the amount of sales as an argument
+# and returns the applicable commission rate.
+
+def determine_comm_rate(sales):
+    #Determind the commission rate.
+    if sales < 10000.00:
+        rate = 0.10
+    elif sales >= 10000 and sales <= 14999.99:
+        rate = 0.12
+    elif sales >= 15000 and sales <= 17999.99:
+        rate = 0.14
+    elif sales >=18000 and sales <= 21999.99:
+        rate = 0.16
+    else:
+        rate = 0.18
+
+    #Return the commission rate.
+        return rate
+
+main()
+
 
         
