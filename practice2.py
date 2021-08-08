2db2b5d4344901f737c52caa00503b44a4987af5
feleshia33 <feleshia.west@g.austincc.edu>
2021-08-08 18:03:02 -0500

    final

--- practice2.py
+++ practice2.py
@@ -2,10 +2,14 @@
 # Depending on whether the number is even or odd,
 # print out an appropriate message.
 
-number = int(input("Enter a number from 1 to 100: "))
+number = int(input("Enter a number from 1 to 1000: "))
 even = number % 2
 
 if even == 0:
     print(number, "is a even nnumber")
 else:
     print(number, "is a odd nnumber")
+
+    
+
+    
