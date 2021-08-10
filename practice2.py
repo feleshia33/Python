b07fe4a1c2319280fd8c8a3bad87d26fd7cd188c
feleshia33 <feleshia.west@g.austincc.edu>
2021-08-09 21:34:56 -0500

    add hypotenuse cal

--- practice2.py
+++ practice2.py
@@ -2,14 +2,32 @@
 # Depending on whether the number is even or odd,
 # print out an appropriate message.
 
-number = int(input("Enter a number from 1 to 1000: "))
-even = number % 2
+#number = int(input("Enter a number from 1 to 1000: "))
+#even = number % 2
 
-if even == 0:
-    print(number, "is a even nnumber")
-else:
-    print(number, "is a odd nnumber")
+#if even == 0:
+#    print(number, "is a even nnumber")
+#else:
+#    print(number, "is a odd nnumber")
 
-    
+
+# This program calcuates the length of a right triangle's
+# hypotenuse
+
+import math
+
+def main():
+    # Get the length of the triangle's two sides
+    a = float(input("Enter the length of side A: "))
+    b = float(input("Enter the length of side B: "))
+
+    #Caculate the length of the hypotenuse
+    c = math.hypot(a,b)
+
+    # Display the length of the hypotenuse
+    print("The length of the hypotenuse is", format(c, ",.2f"))
+
+    #Call function
+main()
 
     
