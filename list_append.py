8aa943bc708db553a8eee90985a304d84cf5c684
feleshia33 <feleshia.west@g.austincc.edu>
2021-08-11 13:01:29 -0500

    list append incompl

--- list_append.py
+++ list_append.py
@@ -1,25 +1,12 @@
+# This program demostrates how to append method
+# can be used to add items to a list
+
 def main():
-    #create empty list
+    # First create an empty list
     name_list = []
 
-    #control loop variable
-    again = 'y'
+    #Create a variable to control the loop
+    again = "y"
 
-    #add names to list
-    while again == 'y':
-        name = input("Enter a name: ")
-
-        #append name to list
-        name_list.append(name)
-        #add another name
-        print("Do you want to add another name? ")
-        again = input("y = yes, anything else = no: ")
-        print()
-    #print names
-    print("Here are the names you entered: ")
-    print()
-    #prints name
-    for name in name_list:
-        print(name)
-
-main()
+    # Add some names to the list
+    
