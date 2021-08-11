d1f8eb41fec84409a0b16a8ab4c5c11f954942b1
feleshia33 <feleshia.west@g.austincc.edu>
2021-08-11 12:57:50 -0500

    in list prog

--- in_list.py
+++ in_list.py
@@ -1,11 +1,17 @@
+# This program demostrates the in operator used with a list
+
 def main():
-    prod_nums = ["V475", "F345", "R345", "P234"]
+    # Create a list of product numbers
+    prod_num = ["V234", "F345", "Q457", "E345"]
 
-    search = input("Enter a product number: ")
-
-    if search in prod_nums:
+    # Get a product number to search for
+    search = input("Enter a product number: ").upper()   
+ 
+    # Determine whether the product number is in the list
+    if search in prod_num:
         print(search, "was found in the list")
     else:
         print(search, "was not found in the list")
 
+    # Call the main function
 main()
