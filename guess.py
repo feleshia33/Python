81f971f8a8cc894f693fc62a70f6ea0a9d8e4165
feleshia33 <feleshiawest@gmail.com>
2021-10-01 16:07:51 -0500

    guessing game

---
+++ guess.py
@@ -0,0 +1,16 @@
+import random
+#This program guesss the random number
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number btw one and 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+    print(f"Yay, congratulations! You've guess the random number of {random_number}!")
+
+guess(10)
+        
