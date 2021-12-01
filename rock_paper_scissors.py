d19f559ed92eac8668eba349fedf50659ce30e2d
feleshia33 <feleshiawest@gmail.com>
2021-12-01 15:05:03 -0600

    added determine winner

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,39 +2,69 @@ import random
 
 # Display instructions
 print("")
-print("The rules of the game is below:")
-print("Paper beats rock, rock beats scissors, scissors beat paper.")
+print("The rules of the game below:")
+print("Paper beats rock, rock beats scissors, scissors beats paper.")
 print("")
 
 # Select player
-print("1) Rock")
-print("2) Paper")
-print("3) Scissors")
-player = input("Please select a player from list: ")
+print("Rock")
+print("Paper")
+print("Scissors")
+print("")
+player = input("Please select from the list: ").lower()
 print("")
 
 # Determine player
 while True:
     if player == "1" or player == "Rock" or player == "rock":
-        print("You have selected Rock!")
+        print("You have selected rock!")
         print("")
         break
     elif player == "2" or player == "Paper" or player == "paper":
-        print("You have selected Paper!")
+        print("You have selected paper!")
         print("")
         break
     elif player == "3" or player == "Scissors" or player == "scissors":
-        print("You have selected Scissors!")
+        print("You have selected scissors!")
         print("")
         break
     else:
         player = input("That is not a valid player, try again: ")
         print("")
 
-computer = ["Rock", "Paper", "Scissors"]
-computer_choice = (random.choice(computer))
 
-print("Computer chose:", computer_choice)
+# Computer choice
+computer = ["Rock", "Paper", "Scissors"]
+computer_choice = (random.choice(computer)).lower()
+
+print("Computer has choosen:" + computer_choice + "!")
+print("")
+
+again = "Y"
+
+# Determine winner
+
+if computer_choice == player:
+    print("It's a tie!")
+    
+elif computer_choice == "rock": 
+    if player == "paper":
+        print("You win!")
+        
+elif computer_choice == "paper": 
+    if player == "scissors":
+        print("You win!")
+        
+elif computer_choice == "scissors":
+    if player == "rock":
+        print("You win")
+       
+else: 
+    print("You lose!")
+    again = input("Play again, enter 'Y' for yes or 'N' for no")
+        
+   
+    
 
 
 
