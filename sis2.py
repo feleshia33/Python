8db0c0fc9b78cb670c6bbc8416f23d118ead0e2c
feleshia33 <feleshiawest@gmail.com>
2021-12-06 17:27:13 -0600

    new progm

---
+++ sis2.py
@@ -0,0 +1,61 @@
+#Toya West
+#Program Status:
+
+
+ 
+def main():
+
+    roomsDic = {}
+    instructorsDic = {}
+    timesDic = {}
+    course = input('Enter a new course number of interest or press enter zero to stop: ')
+ 
+    while course != "0":
+        #get course info
+        rooms = input('Enter the course meeting room: ')
+        instructors = input('Enter course Instructor''s name: ' )
+        times = input('Enter course meeting time: ')
+        print("")
+
+    #add course info to dictionaries
+        
+        roomsDic[course] = [rooms]
+        instructorsDic[course] = [instructors]
+        timesDic[course] = [times]
+
+    #used print to test the dictionaries
+        # print("Course Number(key)  || Room Number(value)")
+        for i in roomsDic:
+            print (i, roomsDic[i])
+
+        # print(roomsDic)
+        # print(instructorsDic)
+        # print(timesDic)
+
+    
+    # for course in rooms:
+    #     print('For Course', course, ':')
+    #     print('The Meeting Room is:', roomsDic[course])
+    #     print('The Instructor is:', instructorsDic[course])
+    #     print('The Meeting Time is:', timesDic[course])
+    # else:
+    #     print(course, 'is an invalid course number.')
+
+    
+#**this was above the second while looped moved for tesing
+
+        #courseinfo = [roomsDic,instructorsDic,timesDic]
+
+
+#TO STOP A WHILE LOOP YOU HAVE TO PLACE THE PRINT OR OTHER STOP IN THE CORRECT LINE IN THE PROGRAM**
+    
+#def courseInfo(roomsDic,instructorsDic,timesDic):
+
+#print(input('Enter a course number of interest or press Enter to stop: '))
+#print('The Meeting Room is:', rooms[course])
+#print('The Meeting Time is:', times[course])
+#print('The Instructor is:', instructors[course])
+
+main()
+
+
